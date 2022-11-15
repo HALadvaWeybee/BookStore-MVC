a15b10883db8598279d65cff4e3dd1c767d81cea
himanshuLadva <himanshu@webtheuniverse.com>
2022-11-15 16:01:27 +0530

    add mvc to the application

--- Web.BookStore/Program.cs
+++ Web.BookStore/Program.cs
@@ -1,6 +1,63 @@
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+/*app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello i am first middleware");
+    await next();
+    await context.Response.WriteAsync("Hello i am first middleware response");
+});
+app.Use(async (context, next) =>
+{   
+    await context.Response.WriteAsync("Hello i am second middleware");
+    await next();
+    await context.Response.WriteAsync("Hello i am second middleware response");
+});
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello i am third middleware");
+    await next();
+    await context.Response.WriteAsync("Hello i am third middleware response");
+});*/
+
+if(app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseRouting();
+
+app.UseEndpoints(endpoint =>
+{
+    endpoint.MapDefaultControllerRoute();
+    /*endpoint.MapGet("/", async context =>
+    {
+       
+        *//*if (app.Environment.IsDevelopment())
+            await context.Response.WriteAsync("hello i am from development");
+        else if (app.Environment.IsProduction())
+            await context.Response.WriteAsync("hello i am from production");
+        else if (app.Environment.IsStaging())
+            await context.Response.WriteAsync("hello i am from staging");
+        // custom environment
+        else if (app.Environment.IsEnvironment("Developing"))
+            await context.Response.WriteAsync("hello i am from Developing");
+
+        await context.Response.WriteAsync(app.Environment.EnvironmentName);*//*
+    });*/
+});
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("/himanshu", async context =>
+    {
+        await context.Response.WriteAsync("Hello Himanshu Ladva!");
+    });
+});
+
 
 app.Run();
